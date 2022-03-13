a6e102a98d09b6c90cabc12f095f301a62fc9a76
Wafa <48634919+Wafa-VI@users.noreply.github.com>
2022-03-13 04:32:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from cs50 import SQL
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -9,20 +9,35 @@ app = Flask(__name__)
 
 db = SQL("sqlite:///score.db")
 
-#db.execute("CREATE TABLE score (id INTEGER, Quizz1 INTEGER, MidTermTest INTEGER, Quizz2 INTEGER, Exam INTEGER")
-db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 1256, 4, 15, 4, 66)
-db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 9876, 3, 20, 5, 65)
-db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 2376, 5, 15, 4, 50)
-db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 2199, 5, 14, 3, 60)
-db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 3301, 3, 18, 4, 55)
-db.execute("UPDATE score SET Total = (SELECT SUM(Quizz1+MidTermTest+Quizz2+Exam) FROM score)")
+ID = [1256, 9876, 2376, 2199, 3301]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "POST":
-        # get id number
-        ids = request.form.get("id")
+        return redirect("/score")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/score", methods=["POST"])
+def score():
+    # get id number
+    db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 1256, 4, 15, 4, 66)
+    db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 9876, 3, 20, 5, 65)
+    db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 2376, 5, 15, 4, 50)
+    db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 2199, 5, 14, 3, 60)
+    db.execute("INSERT INTO score (id, Quizz1, MidTermTest, Quizz2, Exam) VALUES(?, ?, ?, ?, ?)", 3301, 3, 18, 4, 55)
+    db.execute("UPDATE score SET Total = (SELECT SUM(Quizz1+MidTermTest+Quizz2+Exam) FROM score)")
+
+    ids = request.form.get("id")
+
+    if ids not in ID:
+        Max = db.execute("SELECT MAX(Total) FROM score")
+        Min = db.execute("SELECT MIN(Total) FROM score")
+        Avg = db.execute("SELECT AVG(Total) FROM score")
+        return render_template("index.html", Max=Max, Min=Min, Avg=Avg)
+
+    else:
         # convert id to integer
         id = int(ids)
         scoreid = db.execute("SELECT * FROM score WHERE id = ", id)
@@ -43,9 +58,3 @@ def index():
         else:
             Grade = 'F'
             return render_template("scores.html", scoreid=scoreid, Grade=Grade)
-
-    else:
-        Max = db.execute("SELECT MAX(Total) FROM score")
-        Min = db.execute("SELECT MIN(Total) FROM score")
-        Avg = db.execute("SELECT AVG(Total) FROM score")
-        return render_template("index.html", Max=Max, Min=Min, Avg=Avg)
